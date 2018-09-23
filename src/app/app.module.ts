9d609fc0a27679fb28f56ed437bd1abb59ea2716
Kevin Tran <kevin.tran.kt@gmail.com>
2018-09-23 00:13:53 -0700

    Add PWA functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,12 @@ import { AppComponent } from './app.component';
 import { HelloComponent } from './hello/hello.component';
 import { BioComponent } from './bio/bio.component';
 import { ParticlesModule } from 'angular-particle';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent, HelloComponent, BioComponent],
-  imports: [BrowserModule, AppRoutingModule, ParticlesModule],
+  imports: [BrowserModule, AppRoutingModule, ParticlesModule, ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production })],
   providers: [],
   bootstrap: [AppComponent]
 })
